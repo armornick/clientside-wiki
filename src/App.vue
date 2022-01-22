ba9357ad8d1f31df27d72a049b8fe10753efb70d
armornick <nick.arnoeyts@mail.com>
2022-01-22 17:38:24 +0100

    add simple vue app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <main>
+        <h1>Hello, World!</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    html { box-sizing: border-box; }
+    *, *::before, *::after { box-sizing: inherit; }
+
+    main {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        line-height: 1.3;
+        color: #111;
+
+        max-width: 1140px;
+        margin: 0 auto;
+    }
+</style>
+
